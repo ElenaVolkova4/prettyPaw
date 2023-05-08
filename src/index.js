486336403bd316fc4f51ebcb18ece66ce3950765
ElenaVolkova4 <tigra_v_kletke@mail.ru>
2023-05-08 22:25:48 +0300

    added hero styles

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import "./index.html";
+import "swiper/scss";
+import "swiper/scss/pagination";
+
 // все стилевые штуки лучше подключать до импорта стилей
 //new modules
 import "./index.scss";
+
+const videoBG = document.querySelector(".video-bg");
+
+videoBG.innerHTML = `<source set="/video/video.webm" type="video/webm" />
+<source src="/video/video.mp4" type="video/mp4" />
+`;
