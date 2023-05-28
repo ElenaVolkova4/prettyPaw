cae6cdbb3da10f3cd49cb73a26da5d56c5e64c98
ElenaVolkova4 <tigra_v_kletke@mail.ru>
2023-05-28 23:33:31 +0300

    installed swiper

--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ import "./index.scss";
 
 const videoBG = document.querySelector(".video-bg");
 
-videoBG.innerHTML = `<source set="/video/video.webm" type="video/webm" />
+videoBG.innerHTML = `<source src="/video/video.webm" type="video/webm" />
 <source src="/video/video.mp4" type="video/mp4" />
 `;
